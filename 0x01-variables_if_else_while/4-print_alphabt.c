a0fe959207f9661345f3939d247e2701e0335e8c
simo-ngei <simonngei8@gmail.com>
2023-10-06 13:19:10 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,19 +3,20 @@
  *main - entry point
  *Descrption: 'all lower case alphabets exept q and e'
  *Return: always 0
+ */
 int main(void)
 {
-int = 97;
-while (n<=122);
+int n = 97;
+while (n <= 122)
 {
-if (n == 101 || n == 113);
+if (n == 101 || n == 113)
 {
 n++;
 continue;
 }
-puchar(n);
+putchar(n);
 n++;
-putchar(\n)
 }
-return(0)
+putchar('\n');
+return (0);
 }
