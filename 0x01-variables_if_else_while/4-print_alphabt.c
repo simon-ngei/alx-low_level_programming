9fcb89d77c9a12202d0238ad39ebd6ad36915de8
simo-ngei <simonngei8@gmail.com>
2023-10-06 10:56:02 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ *main - entry point
+ *Descrption: 'all lower case alphabets exept q and e'
+ *Return - always 0
+int main(void)
+{
+    int = 97;
+    while (n<=122);
+    {
+    if (n == 101 || n == 113);
+    {
+    n++;
+    continue;
+    }
+    puchar(n);
+    n++;
+    putchar(\n)
+    }
+    return(0)
+}
