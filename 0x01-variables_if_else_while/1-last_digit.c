ebd8595c5fc02c126408a7dbdd5d2a5450116021
simo-ngei <simonngei8@gmail.com>
2023-10-06 13:50:32 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ n = rand() - RAND_MAX / 2;
 ld = n % 10;
 if (ld > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n", n, ld);
+printf("last digit of %d is %d and is greater than 5", n, ld);
 }
 else if (ld == 0)
 {
