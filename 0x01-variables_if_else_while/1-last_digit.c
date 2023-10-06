bc8a1215d4419bc5e1cca0bbc1c81ae3f4a28cf0
simo-ngei <simonngei8@gmail.com>
2023-10-06 12:21:48 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
  *main - main entry
  *
- *Return - always 0
+ *Return: always 0
  /*
 int main(void)
 {
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - main entry
+ *main - entry point
  *Description:'prints the alphabets in lowercase'
- *Return - always 0
+ *Return: always 0
  */
 int main(void)
 {
@@ -12,6 +12,6 @@ while (n <= 122)
 putchar(n);
 n++;
 }
-putchar("\n");
+putchar('\n');
 return (0);
 }
