a21bc3a488bcef8fb8ef1b6a32c0622105a32ccc
simo-ngei <simonngei8@gmail.com>
2023-10-11 05:37:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_alphabet - print lowercase alphabet
+ *
+ *Return:void
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
