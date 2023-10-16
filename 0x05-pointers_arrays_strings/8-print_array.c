0d87dd9ef6aa09b16f8e4ed671c25aec9e64694d
simo-ngei <simonngei8@gmail.com>
2023-10-16 11:34:44 -0700

    ./8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,7 @@ for (index_of_array = 0; index_of_array < n; index_of_array++)
 printf("%d", a[index_of_array]);
 if (index_of_array != (n - 1))
 {
-printf(",");
+printf(", ");
 }
 }
 putchar('\n');
