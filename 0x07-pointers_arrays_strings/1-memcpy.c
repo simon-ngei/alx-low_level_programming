fef916688f940206c4d82f76e8dfdac65503ca9d
simo-ngei <simonngei8@gmail.com>
2023-10-23 03:36:31 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memcpy - copies bytes from memory
+ *@n: function copies
+ *@src: bytes from memory areas
+ *@dest: to memory area
+ *Return: a pointer to @dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
