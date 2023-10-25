976942c8cbd819dc7192f4417dd08695f86767be
simon-ngei <simonngei8@gmail.com>
2023-10-25 11:54:20 -0700

    ./3-factorial

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *is_prime_number - function that returns 1 if the input integer
+ *@n: integer to be checked
+ *Return: 1 for prime number otherwise 0
+ */
+int is_prime_number(int n)
+{
+  
