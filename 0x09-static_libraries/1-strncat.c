d26c05ba6e51518ee35c3b5d2d195936e5240a35
simon-ngei <simonngei8@gmail.com>
2023-10-27 07:39:14 -0700

    0x09-static_libraries

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -8,35 +8,22 @@
  * @n: The maximum number of characters to concatenate
  * Return: dest
  */
-char *_strncat(char *dest, const char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 int len = 0;
 int i;
 
 while (dest[len] != '\0')
+{
 len++;
-{
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[len + i] = src[i];
-/**
- *_strncat - function that concatenates two strings
- *@dest: to be checked
- *@src: to be checked
- *@n: number of bytes to concatenates
- *Return: dest
- */
-char *_strncat(char *dest, char *src, int n)
-{
-int len = strlen(dest);
-int i;
+}
 
-for (i = 0; i < n && *src != '\0'; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
-dest[len + i] = *src;
-src++;
+dest[len + i] = src[i];
 }
+
 dest[len + i] = '\0';
-return (dest);
-}
-}
+
+return dest;
 }
