7bce265578d5f219b89fb87afc4fb8020b7be196
simon-ngei <simonngei8@gmail.com>
2023-11-16 04:04:12 -0800

    0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,10 +8,10 @@ size_t print_list(const list_t *h)
 {
 size_t element;
 element = 0;
-while (h != NULL)
+while (h)
 {
 if (h->str == NULL)
-printf("*[%d] %s\n", 0, "(nil)");
+printf("[0] (nil)\n");
 else
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
