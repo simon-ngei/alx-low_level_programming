448b2dab589b1c0fe2bd299a2e2146f972ecd35c
simon-ngei <simonngei8@gmail.com>
2023-11-23 10:03:56 -0800

    2-get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,23 +2,16 @@
 
 /**
  * get_bit - Gets the value of a bit at a specific index.
- * @number: The input decimal number.
- * @bit_index: The index of the bit to check (0-based).
- *
- * Description: This function retrieves the value
- * within the given decimal number. The index should be in the range [0, 63].
- *
- * Return: The value (0 or 1) of the bit
- *
+ * @n: The input decimal number.
+ * @index: The index of the bit to check (0-based).
+ *Return: value of the bit
  */
-int get_bit(unsigned long int number, unsigned int bit_index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_value;
+int value;
+if (index > 63)
+return (-1);
 
-	if (bit_index > 63)
-		return (-1);
-
-	bit_value = (number >> bit_index) & 1;
-
-	return (bit_value);
+value = (n >> index) & 1;
+return (value);
 }
