d8b27508f2eab0319feb5ec690871eb0a7e48a11
simon-ngei <simonngei8@gmail.com>
2023-11-23 10:44:23 -0800

    5-flip_bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,27 +4,18 @@
  * flip_bits - Count differing bits between two numbers.
  * @n: The first number.
  * @m: The second number.
- *
- * Description:
- * This function calculates the number of bits that need to be flipped to
- * convert one number into another. It performs a bitwise XOR operation to
- * find the differing bits between the numbers and then counts the number
- * of set (1) bits in the result.
- *
- * Return:
- * The count of differing bits, representing the number of bits that
- * need to be flipped to transition from 'n' to 'm'.
+ *Return: number of bits to change
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor_result = n ^ m; /* Find differing bits. */
-	unsigned int bit_diff_count = 0; /* Initialize the count. */
-
-	while (xor_result != 0)
-	{
-		bit_diff_count += xor_result & 1; /* Count each set bit. */
-		xor_result >>= 1; /* Shift right to check the next bit. */
-	}
-
-	return (bit_diff_count); /* Return the count of differing bits. */
+int j, count = 0;
+unsigned long int current;
+unsigned long int exclusive = n ^ m;
+for (j = 63; j >= 0; j--)
+{
+current = exclusive >> j;
+if (current & 1)
+count++;
+}
+return (count);
 }
